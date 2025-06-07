cde4f7ffd1a13a52ab61198425790f8b48b86801
ELAINERAFAEL <162643406+ELAINERAFAEL@users.noreply.github.com>
2025-06-06 21:43:22 -0300

    Criando

---
+++ novidades.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Novidades</title>
+</head>
+<body>
+    <?php
+       include ('pages/header.php')
+    ?>
+    <?php
+    include ('pages/footer.php')
+    ?>
+</body>
+</html>
