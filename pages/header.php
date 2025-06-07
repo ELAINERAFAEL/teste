1bfc171a5bce83814d28dabb00578b73231c4ce6
ELAINERAFAEL <162643406+ELAINERAFAEL@users.noreply.github.com>
2025-06-06 21:42:59 -0300

    Ajustando include

---
+++ pages/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<header>
+        <a href="index.html" title="Home">
+            <img src="imagem/site.jpg" alt="Banner" title="Banner" class="imagem-redonda">
+        </a>
+    
+        <nav>
+        <a href="index.php" title="Home">Home</a>
+        <a href="produtos.php" title="Produtos">Produtos</a>
+        <a href="Sobre.php" title="Sobre">Sobre</a>
+        <a href="contato.php" title="Contato">Contato</a>
+        <a href="novidades.php" title="Novidades">Novidades</a>
+
+
+        </nav>
+    </header>   
